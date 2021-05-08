2bb5b663a0ee2a2e3cdba833116a0f1ff478a020
SweetOBurritO <jarredchanner@gmail.com>
2021-05-08 14:36:49 +0200

    Added insert document to DatabaseConnection - Added error handeling to mongo functions

--- DatabaseConnection/index.js
+++ DatabaseConnection/index.js
@@ -1,9 +1,11 @@
 const findAll = require('./findAll');
 const getCollectionByFilter = require('./getCollectionByFilter');
+const insertDocument = require('./insertDocument');
 
 const DatabaseConnection = {
   findAll,
   getCollectionByFilter,
+  insertDocument,
 };
 
 module.exports = DatabaseConnection;
