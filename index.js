ea5eb58613cbaf343d26d098720d4b4c2886530e
SweetOBurritO <jarredchanner@gmail.com>
2021-05-12 21:29:07 +0200

    Updated packages for handeling form data

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const apiRoutes = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
