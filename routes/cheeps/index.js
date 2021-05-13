04fb8a4d65f3ddd4ac7c4e9eece893456664f510
SweetOBurritO <jarredchanner@gmail.com>
2021-05-13 17:31:54 +0200

    Finished update and delete routes

--- routes/cheeps/index.js
+++ routes/cheeps/index.js
@@ -24,23 +24,26 @@ cheeps.get('/:id', async (req, res) => {
 cheeps.post('/', async (req, res) => {
   const {text, date, username, userImageUrl} = req.body;
   const cheep = new Cheep(text, date, username, userImageUrl);
-
   const success = await cheepController.insert(cheep);
+  const response = new Response(200, {success: success}, null );
 
-  const response = new Response(200, {sucess: success}, null );
   res.send(response.toResponseObject());
 });
 
 cheeps.put('/:id', async (req, res) => {
   const {id} = req.params;
+  const updateValues = req.body;
+  const success = await cheepController.update(id, updateValues);
+  const response = new Response(200, {success: success}, null );
 
-  const response = new Response(200, `Cheeps: update id: ${id}`, null );
   res.send(response.toResponseObject());
 });
 
 cheeps.delete('/:id', async (req, res) => {
   const {id} = req.params;
-  const response = new Response(200, `Cheeps: delete id: ${id}`, null );
+  const success = await cheepController.delete(id);
+  const response = new Response(200, {success: success}, null );
+
   res.send(response.toResponseObject());
 });
 
