99aaa963f32699462a0b0c6c681a77b64d1f3337
SweetOBurritO <jarredchanner@gmail.com>
2021-05-17 21:10:57 +0200

    Updated database constants

--- constants/databaseConstants.js
+++ constants/databaseConstants.js
@@ -1,16 +1,11 @@
 const DB_URL = process.env.DB_URL;
 
-const DB_SETTINGS = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+const DB_SETTINGS = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true };
 
-const DB_NAME = 'dbChirper';
 
 const Database = {
   DB_URL,
-  DB_SETTINGS,
-  DB_NAME,
+  DB_SETTINGS
 };
 
 module.exports = Database;
