84efebb47ebab401ca050962aefc4b323aa4932d
shaikjeem <mohammeds@bbd.co.za>
2021-05-20 16:31:46 +0200

    #home: updated UX

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -11,18 +11,22 @@ export const Home = View({
     data: {
         cheep: Cheep,
         trends: Trends,
+        promptCheep: 'Cheep, Cheep!',
         props: [
             {
+                profileImage: 'https://picsum.photos/id/23/200',
                 handle: '@realDonaldTrump',
                 heading: 'My Button is Bigger than Yours',
                 body: 'North Korean'
             },
             {
+                profileImage: 'https://picsum.photos/id/23/200',
                 handle: '@realDonaldTrump',
                 heading: 'Nominate Me!',
                 body: 'Lowest rated Oscars in HISTORY. Problem is, we don\'t have Stars anymore - except your President (just kidding, of course)!'
             },
             {
+                profileImage: 'https://picsum.photos/id/23/200',
                 handle: '@realDonaldTrump',
                 heading: 'Nominate Me!',
                 body: 'Lowest rated Oscars in HISTORY. Problem is, we don\'t have Stars anymore - except your President (just kidding, of course)!'
